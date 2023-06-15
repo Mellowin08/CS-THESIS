6c980fe1c09d5d1ffb4e7eae699d73c4e7b52136
Mellowin08 <jester10138@gmail.com>
2023-06-15 17:47:15 +0800

    init

--- Practice code/music_controller/frontend/src/components/HomePage.js
+++ Practice code/music_controller/frontend/src/components/HomePage.js
@@ -19,7 +19,7 @@ export default class HomePage extends Component {
         return (
         <Router>
             <Switch>
-                <Route path='/'><p>This is the home page</p></Route>
+                <Route exact path='/'><h1>This is the home page</h1></Route>
                 <Route path='/join' component={RoomJoinPage}/>
                 <Route path='/create' component={CreateRoomPage}/>
             </Switch>
