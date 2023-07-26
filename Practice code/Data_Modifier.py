7c26504989bf8c901be239d4bc366142f373b359
Mellowin08 <jester10138@gmail.com>
2023-07-27 00:14:13 +0800

    init

--- Practice code/Data_Modifier.py
+++ Practice code/Data_Modifier.py
@@ -6,7 +6,7 @@ def modify_csv(input_file, output_file, num_rows_to_keep):
     df.to_csv(output_file, index=False)
 
 if __name__ == "__main__":
-    input_file = r'D:\Documents\Reviews.csv'
+    input_file = r'D:\Downloads\Reviews.csv'
     output_file = "Training Data.csv"
-
+    num_rows_to_keep = 500
     modify_csv(input_file, output_file, num_rows_to_keep)
